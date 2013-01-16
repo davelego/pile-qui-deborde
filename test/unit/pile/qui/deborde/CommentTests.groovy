6a1236fd5c24541ef6e1dd5cfdc788eddd7ed830
unknown <charette@a210pc012.rcisima.isima.fr>
2013-01-16 17:44:38 +0100

    Ajout des tests unitaires

---
+++ test/unit/pile/qui/deborde/CommentTests.groovy
@@ -0,0 +1,12 @@
+package pile.qui.deborde
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+
+@TestFor(Comment)
+class CommentTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
