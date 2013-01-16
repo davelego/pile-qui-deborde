c0f14591dcec7aa6595d177c2c90bd848b9ae3d4
unknown <charette@a210pc012.rcisima.isima.fr>
2013-01-16 14:59:53 +0100

    Test Rebuild Project

--- test/unit/pile/qui/deborde/HelloWorldControllerTests.groovy
+++ test/unit/pile/qui/deborde/HelloWorldControllerTests.groovy
@@ -1,8 +1,5 @@
 package pile.qui.deborde
 
-import grails.test.mixin.*
-import org.junit.*
-
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
