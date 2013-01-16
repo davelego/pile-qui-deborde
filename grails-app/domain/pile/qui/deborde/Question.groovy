e1a928969f8b09e71a42a853676173aab0185c8a
unknown <charette@a210pc012.rcisima.isima.fr>
2013-01-16 16:47:20 +0100

    Change Permission and add domains

--- grails-app/domain/pile/qui/deborde/Question.groovy
+++ grails-app/domain/pile/qui/deborde/Question.groovy
@@ -1,7 +1,21 @@
 package pile.qui.deborde
 
 class Question {
-
+	
+	String title
+	String body
+	
+	int notationUp = 0
+	int notationDown = 0 
+	
+	//List<Answer> answers
+	//List<Comment> comments
+	//List<Tags> tags
+	static hasMany = [answers:Answer, comments:Comment, tags:Tags]
+	
+	//Author author
+	static belongsTo = [author:Author]
+	
     static constraints = {
     }
 }
