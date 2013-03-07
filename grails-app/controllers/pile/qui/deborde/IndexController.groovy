2adbd48d1c0908ef3ed7c17ab5aa71b0a9325b34
david <dav.c@hotmail.fr>
2013-03-07 09:34:37 +0100

    Ajout d'un controller Index, acces au demarrage de l'app grace a UrlMapping + questions sur page d'accueil

---
+++ grails-app/controllers/pile/qui/deborde/IndexController.groovy
@@ -0,0 +1,12 @@
+package pile.qui.deborde
+
+class IndexController {
+
+	/**
+	 * First method called at the beginning of the app
+	 * @return
+	 */
+    def index() { 
+		render(view:"index", model:[questions:Question.getAll()])
+	}
+}
---
+++ test/unit/pile/qui/deborde/IndexControllerTests.groovy
@@ -0,0 +1,16 @@
+package pile.qui.deborde
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(IndexController)
+class IndexControllerTests {
+
+    void testSomething() {
+    }
+}
